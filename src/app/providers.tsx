3f771146cfbe67639332f073af9ace8a06b420e0
Damian <d.rogalski.417@studms.ug.edu.pl>
2026-01-05 21:56:37 +0100

    feat: adjust page views and providers

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,12 +1,28 @@
 "use client";
 
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { SessionProvider } from "next-auth/react";
 import { SidebarProvider } from "@/app/core/contexts/sidebar-context";
+import { useState } from "react";
 
 export function Providers({ children }: { children: React.ReactNode }) {
+    const [queryClient] = useState(
+        () =>
+            new QueryClient({
+                defaultOptions: {
+                    queries: {
+                        staleTime: 60 * 1000,
+                        refetchOnWindowFocus: false,
+                    },
+                },
+            })
+    );
+
     return (
-        <SessionProvider>
-            <SidebarProvider>{children}</SidebarProvider>
-        </SessionProvider>
+        <QueryClientProvider client={queryClient}>
+            <SessionProvider>
+                <SidebarProvider>{children}</SidebarProvider>
+            </SessionProvider>
+        </QueryClientProvider>
     );
 }
