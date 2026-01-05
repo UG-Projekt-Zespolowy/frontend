9ddf110bbd3590d901a7b3584da66e4104698ddb
Damian <d.rogalski.417@studms.ug.edu.pl>
2026-01-05 21:54:29 +0100

    feat: add core types

---
+++ src/app/core/types/index.ts
@@ -0,0 +1,5 @@
+export type { PageResponse } from "@/app/core/types/common.types";
+export type { User } from "@/app/core/types/user.types";
+export type { Issue } from "@/app/core/types/issue.types";
+export type { Epic } from "@/app/core/types/epic.types";
+export type { Project } from "@/app/core/types/project.types";
