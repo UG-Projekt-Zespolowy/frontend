cf16a84af076cdd1404e709c7480a737808dd611
Damian <d.rogalski.417@studms.ug.edu.pl>
2026-01-07 22:22:35 +0100

    feat: fix typing

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -1,6 +1,7 @@
 declare module "next-auth" {
     interface Session {
         accessToken?: string;
+        idToken?: string;
     }
 
     interface User {
@@ -11,5 +12,6 @@ declare module "next-auth" {
 declare module "next-auth/jwt" {
     interface JWT extends DefaultJWT {
         accessToken?: string;
+        idToken?: string;
     }
 }
