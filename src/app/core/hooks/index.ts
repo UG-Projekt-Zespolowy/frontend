8045c28d0d4c7273858b38e7fb505e033475a9aa
mkurzych <marta.kurzych@wp.pl>
2026-01-10 18:40:53 +0100

    refactor: removed unused functions

--- src/app/core/hooks/index.ts
+++ src/app/core/hooks/index.ts
@@ -1,5 +1,4 @@
 export * from "./use-issues";
 export * from "./use-epics";
 export * from "./use-projects";
-export * from "./use-users";
 export type { User, Issue, Epic, Project, PageResponse } from "@/app/core/types";
